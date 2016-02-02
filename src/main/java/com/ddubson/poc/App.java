955f51315ef0735f5149a9ca52298cbff99a3c01
Dmitriy Dubson <d.dubson@gmail.com>
2016-02-01 19:49:26 -0500

    Initial Import.

---
+++ src/main/java/com/ddubson/poc/App.java
@@ -0,0 +1,13 @@
+package com.ddubson.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
